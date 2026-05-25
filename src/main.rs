0e66d5e0515424c46bedc9792af285cb8dfc3720
a.volkov <a.volkov111555@gmail.com>
2026-05-25 23:28:17 +0300

    Moving auxiliary thread in worker.rs.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,10 @@
 use crossbeam_channel::{Receiver, Sender, bounded};
 use eframe::egui::{self};
 use egui_plot::{Line, Plot, PlotBounds, PlotPoint, PlotPoints};
-use std::f64::consts::PI;
-use std::sync::{
-    Arc, Mutex,
-    atomic::{AtomicBool, Ordering},
-};
-use std::thread::{self, JoinHandle};
-use std::time::{Duration, Instant};
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
+
+mod worker;
 
 const WINDOW_SECONDS: f64 = 3600.0;
 
@@ -37,8 +34,7 @@ struct MyApp {
     follow_latest: bool,
     last_plot_x: f64,
 
-    worker: Option<JoinHandle<()>>,
-    running: Arc<AtomicBool>,
+    worker: worker::Worker,
 }
 
 impl MyApp {
@@ -61,83 +57,8 @@ impl MyApp {
             follow_latest: true,
             last_plot_x: 0.0,
 
-            worker: None,
-            running: Arc::new(AtomicBool::new(false)),
+            worker: worker::Worker::new(),
         }
-    }
-
-    fn start_worker(&mut self) {
-        if self.worker.is_some() {
-            return;
-        }
-
-        self.running.store(true, Ordering::Release);
-
-        let command_receiver = self.command_receiver.clone();
-        let response_sender = self.response_sender.clone();
-
-        let running = self.running.clone();
-        let points = self.points.clone();
-
-        self.worker = Some(thread::spawn(move || {
-            const POLL_INTERVAL: Duration = Duration::from_millis(100);
-            let start_time = Instant::now();
-            let mut next_poll = Instant::now() + POLL_INTERVAL;
-
-            while running.load(Ordering::Acquire) {
-                let now = Instant::now();
-
-                if now >= next_poll {
-                    let delta_t = start_time.elapsed().as_secs_f64();
-
-                    let sinus_sum: f64 = (1..=10_000)
-                        .step_by(2)
-                        .map(|i| {
-                            let i = i as f64;
-                            4.0 * 100.0 / PI / i * (2.0 * PI * i * delta_t / 400.0).sin()
-                        })
-                        .sum();
-
-                    if let Ok(mut points) = points.lock() {
-                        points.push(PlotPoint {
-                            x: delta_t,
-                            y: sinus_sum,
-                        });
-                    }
-                    next_poll += POLL_INTERVAL;
-
-                    if Instant::now() > next_poll + POLL_INTERVAL {
-                        next_poll = Instant::now() + POLL_INTERVAL;
-                    }
-                    continue;
-                }
-
-                let timeout = next_poll.saturating_duration_since(now);
-
-                match command_receiver.recv_timeout(timeout) {
-                    Ok(command) => {
-                        let response = format!("You send: {}", command);
-                        let _ = response_sender.send(response);
-                    }
-
-                    Err(_) => {}
-                }
-            }
-        }));
-    }
-
-    fn stop_worker(&mut self) {
-        self.running.store(false, Ordering::Release);
-
-        if let Some(handle) = self.worker.take() {
-            let _ = handle.join();
-        }
-    }
-}
-
-impl Drop for MyApp {
-    fn drop(&mut self) {
-        self.stop_worker();
     }
 }
 
@@ -150,11 +71,15 @@ impl eframe::App for MyApp {
         egui::CentralPanel::default().show_inside(ui, |ui| {
             ui.horizontal(|ui| {
                 if ui.button("Start").clicked() {
-                    self.start_worker();
+                    self.worker.start(
+                        self.command_receiver.clone(),
+                        self.response_sender.clone(),
+                        self.points.clone(),
+                    );
                 }
 
                 if ui.button("Stop").clicked() {
-                    self.stop_worker();
+                    self.worker.stop();
                 }
 
                 if ui.button("Clear").clicked() {
