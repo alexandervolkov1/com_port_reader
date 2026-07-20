7c7b3efb35dc13c55a36255c41c738ca15d5108f
alexandervolkov1 <a.volkov111555@gmail.com>
2026-07-20 17:08:03 +0300

    update command view & model

--- src/components/command_model.rs
+++ src/components/command_model.rs
@@ -2,10 +2,10 @@ use crate::data::Signal;
 use crossbeam_channel::{Receiver, Sender};
 
 pub struct CommandModel {
-    pub command_sender: Sender<Signal>,
-    pub response_receiver: Receiver<String>,
-    pub command_buffer: String,
-    pub last_response: String,
+    command_sender: Sender<Signal>,
+    response_receiver: Receiver<String>,
+    command_buffer: String,
+    last_response: String,
 }
 
 impl CommandModel {
@@ -18,9 +18,31 @@ impl CommandModel {
         }
     }
 
+    pub fn command_buffer_mut(&mut self) -> &mut String {
+        &mut self.command_buffer
+    }
+
+    pub fn last_response(&self) -> &str {
+        &self.last_response
+    }
+
     pub fn update(&mut self) {
         if let Ok(response) = self.response_receiver.try_recv() {
             self.last_response = response;
         }
+    }
+
+    pub fn submit(&mut self) {
+        match Signal::from_string(&self.command_buffer) {
+            Ok(signal) => {
+                let _ = self.command_sender.send(signal);
+            }
+
+            Err(e) => {
+                self.last_response = e;
+            }
+        }
+
+        self.command_buffer.clear();
     }
 }
--- src/components/command_view.rs
+++ src/components/command_view.rs
@@ -1,26 +1,17 @@
-use crate::{components::command_model::CommandModel, data::Signal};
+use crate::components::command_model::CommandModel;
 use eframe::egui;
 
 pub fn show(ui: &mut egui::Ui, command: &mut CommandModel) {
     ui.horizontal(|ui| {
         ui.label("Command:");
 
-        let response = ui.text_edit_singleline(&mut command.command_buffer);
+        let response = ui.text_edit_singleline(command.command_buffer_mut());
 
         if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
-            match Signal::from_string(&command.command_buffer) {
-                Ok(signal) => {
-                    let _ = command.command_sender.send(signal);
-                }
-                Err(e) => {
-                    command.last_response = e;
-                }
-            }
-
-            command.command_buffer.clear();
+            command.submit();
             response.request_focus();
         }
     });
 
-    ui.label(&command.last_response);
+    ui.label(command.last_response());
 }
